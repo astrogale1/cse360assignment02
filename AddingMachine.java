03af3054101fe621097c5014121b8bf996c47303
astrogale1 <57536815+astrogale1@users.noreply.github.com>
2020-10-06 11:57:09 -0700

    Second Version of AddingMachine
    Uses the updated changes, with the revised functionality of the clear() method.

--- AddingMachine.java
+++ AddingMachine.java
@@ -7,46 +7,55 @@ package cse360assignment02;
  */
 public class AddingMachine {
 	  private int total;
+	  private String memory;
 	  
 	  /**
 	   * Adding Machine Constructor
 	   */
 	  public AddingMachine () {
 	    total = 0;  // not needed - included for clarity
+	    memory = "0";
 	  }
 	  
 	  /**
 	   * Getter accessor for private variable 'total'
-	   * @return
+	   * @return total	integer value representing the culmination of prior operations
 	   */
 	  public int getTotal () {
-	    return 0;
+	    return total;
 	  }
 	  
 	  /**
 	   * Adds given value to the internal total value
-	   * @param value
+	   * @param value	integer value to be added to internal value 'total'
 	   */
 	  public void add (int value) {
+		  total = total + value;
+		  memory = memory.concat(" + ".concat(Integer.toString(value)));
 	  }
 
 	  /**
 	   * Subtracts given value from the internal total value
-	   * @param value
+	   * @param value	integer value to be subtracted from internal value 'total'
 	   */
 	  public void subtract (int value) {
+		  total = total - value;
+		  memory = memory.concat(" - ".concat(Integer.toString(value)));
 	  }
 
 	  /**
 	   * Returns the stored total as a string
+	   * @return memory	String value showcasing past operations
 	   */
 	  public String toString () {
-	    return "";
+	    return memory;
 	  }
 
 	  /**
-	   * Resets the total back to 0
+	   * Resets the total and the memory
 	   */
 	  public void clear() {
+		  total = 0;
+		  memory = Integer.toString(total);
 	  }
 }

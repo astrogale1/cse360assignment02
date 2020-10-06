a32983a0d3e44044c9f1711a5cd30d38a12d8003
astrogale1 <57536815+astrogale1@users.noreply.github.com>
2020-10-05 18:03:40 -0700

    Initial Version of AddingMachine
    Initial Version, will be modified

---
+++ AddingMachine.java
@@ -0,0 +1,52 @@
+package cse360assignment02;
+
+/**
+ * A class that stores an internal value and allows simplistic operations on said value.
+ * @author astrogale1
+ *
+ */
+public class AddingMachine {
+	  private int total;
+	  
+	  /**
+	   * Adding Machine Constructor
+	   */
+	  public AddingMachine () {
+	    total = 0;  // not needed - included for clarity
+	  }
+	  
+	  /**
+	   * Getter accessor for private variable 'total'
+	   * @return
+	   */
+	  public int getTotal () {
+	    return 0;
+	  }
+	  
+	  /**
+	   * Adds given value to the internal total value
+	   * @param value
+	   */
+	  public void add (int value) {
+	  }
+
+	  /**
+	   * Subtracts given value from the internal total value
+	   * @param value
+	   */
+	  public void subtract (int value) {
+	  }
+
+	  /**
+	   * Returns the stored total as a string
+	   */
+	  public String toString () {
+	    return "";
+	  }
+
+	  /**
+	   * Resets the total back to 0
+	   */
+	  public void clear() {
+	  }
+}
